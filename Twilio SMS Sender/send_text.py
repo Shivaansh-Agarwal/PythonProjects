1032e885066e361a636d95f8f09c733b80bfa58e
Shivaansh Agarwal <shivanshagrawal1997@gmail.com>
2018-10-24 19:17:48 +0530

    Update send_text.py

--- Twilio SMS Sender/send_text.py
+++ Twilio SMS Sender/send_text.py
@@ -1,9 +1,9 @@
 from twilio.rest import Client
 
 # Your Account SID from twilio.com/console
-account_sid = "AC6c888236aacbf1fe5d289d36727d9f03"
+account_sid = "AC6c888236aacbf1fe5d289d##########"
 # Your Auth Token from twilio.com/console
-auth_token  = "e2e640b516a96ec17b27ad880172XXXX"
+auth_token  = "e2e640b516a96ec17b27ad##########"
 
 client = Client(account_sid, auth_token)
 
